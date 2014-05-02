334c98125ef2ab8abd1df8cf0dddd4165e2f5d4e
Charles Sprayberry <cspray@gmail.com>
2014-05-01 22:42:05 -0400

    adding docs

--- config/bootstraps.php
+++ config/bootstraps.php
@@ -1,6 +1,7 @@
 <?php
 
 /**
+ * Setup or run the appropriate actions at application startup time.
  * 
  * @license See LICENSE in source root
  * @version 1.0
@@ -17,6 +18,9 @@ return function(Injector $injector, Config $config) {
 
     (new EnvironmentConfigBootstrap($config))->run();
     if ($config['ini'] instanceof Config) { (new IniSetBootstrap($config['ini']))->run(); }
+
+    // if you don't run this bootstrap we assume you're have your own method
+    // of adding routes to the $router; otherwise Labrador won't do anything!
     $router = $injector->make('Labrador\\Router\\FastRouteRouter');
     (new ConfigRoutesBootstrap($config, $router))->run();
 
