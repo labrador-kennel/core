5103e73c53b926096c125d96c66c3d982cb24dfd
Charles Sprayberry <cspray@gmail.com>
2014-05-14 07:36:58 -0400

    cleaning up some docs and a path change

--- config/master_config.php
+++ config/master_config.php
@@ -3,7 +3,7 @@
 /**
  * This is the master configuration for the Labrador library; you can also use
  * this file to set any configuration values that should just always be set for
- * your application.
+ * your application regardless of environment.
  *
  * @license See LICENSE in source root
  * @version 1.0
@@ -25,20 +25,15 @@ return function(Config $config) {
     /**
      * ConfigDirective::ENVIRONMENT       string
      *
-     * Determines the config file that is ran for a given request; e.g. if the
-     * value is set to 'development' than '/config/development/config.php' is
-     * ran by default. Labrador is "aware" of the environment 'production'. If a
-     * value is set here but is not available in the specified configuration directory
-     * then the environment will default to 'production'.
-     *
-     * @todo We don't actually have a test case covering this
+     * Determines the environment that the current application is running in. This
+     * is a completely arbitrary string and only holds meaning to your application.
      */
     $config[ConfigDirective::ENVIRONMENT] = 'development';
 
     /**
      * ConfigDirective::ROOT_DIR          string
      *
-     * The root directory that Labrador and app files live under.
+     * The root directory that Labrador files live under.
      */
     $config[ConfigDirective::ROOT_DIR] = dirname(__DIR__);
 
@@ -64,7 +59,7 @@ return function(Config $config) {
      * A callback accepting a Auryn\Provider as the only argument. The function
      * should set the appropriate dependencies and services needed by the application.
      */
-    $config[ConfigDirective::SERVICE_REGISTERS_CALLBACK] = include $config[ConfigDirective::CONFIG_DIR] . '/service_registers.php';
+    $config[ConfigDirective::SERVICE_REGISTERS_CALLBACK] = include $config[ConfigDirective::CONFIG_DIR] . '/services.php';
 
     /**
      * ConfigDirective::BOOTSTRAPS_CALLBACK               callable
