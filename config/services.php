8bab9d5cc986e02d4b4c47cd90bdad0209288657
Charles Sprayberry <cspray@gmail.com>
2014-05-14 07:38:54 -0400

    rename to service_registers.php -> services.php
    This was done because there’s nothing saying that only registers can be
    used in this callback file.
    Instead you just need to add services, it does not matter how you do
    that.

--- config/services.php
+++ config/services.php
@@ -10,14 +10,20 @@
  */
 
 use Labrador\Service\DefaultServicesRegister;
+use Labrador\ConfigDirective;
 use Auryn\Injector;
+use Configlet\Config;
 
 /**
  * This function should set services against the Auryn\Injector; it should not
  * actually preform any actions on services or do anything other than set an
  * object graph and its dependencies.
  */
-return function(Injector $injector) {
+return function(Injector $injector, Config $config) {
+
+    $demoTemplates = $config[ConfigDirective::ROOT_DIR] . '/src/LabradorDemo/_templates';
+    $docDir = $config[ConfigDirective::ROOT_DIR] . '/doc';
+    (new \LabradorDemo\Service\ControllerRegister($demoTemplates, $docDir))->register($injector);
 
     // This service register MUST be ran OR you MUST provide an instance of
     // Labrador\Application to the $provider with appropriate dependencies defined
