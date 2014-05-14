4d090024a9ed335a2e0ac1986ceb920c744712f3
Charles Sprayberry <cspray@gmail.com>
2014-05-14 07:39:10 -0400

    setting basic error and exception handler

--- init.php
+++ init.php
@@ -8,6 +8,28 @@
  * @since   0.1
  */
 
+set_error_handler(function($severity, $msg, $file, $line) {
+    throw new ErrorException($msg, 0, $severity, $file, $line);
+});
+set_exception_handler(function(Exception $exception) {
+    http_response_code(500);
+    $msg = htmlspecialchars($exception->getMessage());
+    echo <<<HTML
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Labrador Error</title>
+    </head>
+    <body>
+        <h1>Internal Server Error</h1>
+        <p>An error was encountered processing your request. Please contact the administrator.</p>
+        <p>The error message was: {$msg}</p>
+    </body>
+</html>
+HTML;
+    exit;
+});
+
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Labrador\Bootstrap\FrontControllerBootstrap;
