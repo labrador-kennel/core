852ade429395cbff9814b74e206663b74dcc5e47
Charles Sprayberry <cspray@gmail.com>
2014-05-14 07:36:33 -0400

    removing environment config reference

--- config/bootstraps.php
+++ config/bootstraps.php
@@ -16,10 +16,11 @@ use Auryn\Injector;
 
 return function(Injector $injector, Config $config) {
 
-    (new EnvironmentConfigBootstrap($config))->run();
-    if ($config['ini'] instanceof Config) { (new IniSetBootstrap($config['ini']))->run(); }
+    if ($config['ini'] instanceof Config) {
+        (new IniSetBootstrap($config['ini']))->run();
+    }
 
-    // if you don't run this bootstrap we assume you're have your own method
+    // if you don't run this bootstrap we assume you have your own method
     // of adding routes to the $router; otherwise Labrador won't do anything!
     $router = $injector->make('Labrador\\Router\\FastRouteRouter');
     (new ConfigRoutesBootstrap($config, $router))->run();
