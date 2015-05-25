4622686fa83fb7740d62880fc6df4cb1f2556d8c
Charles Sprayberry <charles@designbook.com>
2015-05-25 15:39:49 -0400

    adds test for services object

---
+++ test/Labrador/ServicesTest.php
@@ -0,0 +1,23 @@
+<?php
+
+
+namespace Labrador;
+
+use Auryn\Injector;
+use PHPUnit_Framework_TestCase as UnitTestCase;
+
+class ServicesTest extends UnitTestCase {
+
+    public function testInjectorInstanceCreated() {
+        $injector = (new Services())->createInjector();
+
+        $this->assertInstanceOf(Injector::class, $injector);
+    }
+
+    public function testInjectorCreatesEngine() {
+        $injector = (new Services())->createInjector();
+
+        $this->assertInstanceOf(CoreEngine::class, $injector->make(CoreEngine::class));
+    }
+
+}
