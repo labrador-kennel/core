c8fff1f7906b8137bbb21b5f2998fc115a913bfc
Charles Sprayberry <charles@designbook.com>
2015-05-25 12:28:05 -0400

    initial commit

---
+++ src/Labrador/Collection/SafeHashMap.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * A HashMap that will return null if the key requested was not found.
+ *
+ * @license See LICENSE in source root
+ * @version 1.0
+ * @since   1.0
+ */
+
+namespace Labrador\Collection;
+
+use Collections\HashMap;
+
+class SafeHashMap extends HashMap {
+
+}
